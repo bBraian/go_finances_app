29775b03d955fd07a0549e18fe3b5afe26005d05
bBraian <braianvoficial@gmail.com>
2023-02-14 21:12:33 -0300

    updating logo

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -10,7 +10,7 @@ import {
 } from './styles';
 
 import GoogleSvg from '../../assets/google.svg';
-import LogoSvg from '../../assets/logo.svg';
+import LogoSvg from '../../../assets/logo.svg';
 import AppleSvg from '../../assets/apple.svg';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { SignInButton } from '../../components/SignInButton';
@@ -44,7 +44,7 @@ export function SignIn() {
                 <TitleWrapper>
                     <LogoSvg 
                         width={RFValue(120)}
-                        height={RFValue(68)}
+                        height={RFValue(80)}
                     />
 
                     <Title>

50d4d718d508f67d2277eb3de046b63a123faa42
bBraian <braianvoficial@gmail.com>
2023-02-20 19:59:08 -0300

    updating

--- android/app/src/main/java/com/gofinances/MainActivity.java
+++ android/app/src/main/java/com/gofinances/MainActivity.java
@@ -1,4 +1,4 @@
-package com.gofinances;
+package com.bbraiandev.gofinances;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/gofinances/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/gofinances/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.gofinances.newarchitecture.modules;
+package com.bbraiandev.gofinances.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
